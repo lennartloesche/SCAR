7a95516d7143eb200eceb8a52d0abc16577d44d6
Lennart <loesche.lennart@gmail.com>
2021-03-08 12:12:13 +0100

    Config Fix
    Config Fix -> Channel & Rollen

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const ms = require("ms");
 const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 // ❯ Verbindung zur Config
-// const config = require("./config.json");
+const config = require("./config.json");
 
 // ❯ Rich Presence/RPC
 setInterval(() => {
